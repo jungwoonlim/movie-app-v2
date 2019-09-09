0d97d19476a1814077e593e73600a22fbfaefc7b
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-09-09 17:12:51 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React from "react";
-
-const Food = ({ name, pic }) => {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={pic} alt={name} />
-    </div>
-  );
-};
+import PropTypes from "prop-types";
 
 const foodLike = [
   {
     id: "1",
     name: "One",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 1.1
   },
   {
     id: "2",
     name: "Two",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 2.2
   },
   {
     id: "3",
     name: "Three",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 3.3
   }
 ];
+
+const Food = ({ name, pic, rating }) => {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={pic} alt={name} />
+    </div>
+  );
+};
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 class App extends React.Component {
   render() {
@@ -36,7 +47,12 @@ class App extends React.Component {
       <div className="App">
         <h1>Hello React</h1>
         {foodLike.map(dish => (
-          <Food key={dish.id} name={dish.name} pic={dish.image}></Food>
+          <Food
+            key={dish.id}
+            name={dish.name}
+            pic={dish.image}
+            rating={dish.rating}
+          ></Food>
         ))}
       </div>
     );
