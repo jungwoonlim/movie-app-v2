9c5511e87f723fa8a454daf3181902dd6fba9ae9
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-09-09 17:56:43 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -1,59 +1,23 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-const foodLike = [
-  {
-    id: "1",
-    name: "One",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 1.1
-  },
-  {
-    id: "2",
-    name: "Two",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 2.2
-  },
-  {
-    id: "3",
-    name: "Three",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 3.3
-  }
-];
-
-const Food = ({ name, pic, rating }) => {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={pic} alt={name} />
-    </div>
-  );
-};
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
+// import PropTypes from "prop-types";
 
 class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello React</h1>
-        {foodLike.map(dish => (
-          <Food
-            key={dish.id}
-            name={dish.name}
-            pic={dish.image}
-            rating={dish.rating}
-          ></Food>
-        ))}
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
