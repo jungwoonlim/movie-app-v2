f4ff7c460e7f3c4ad13cbbc1fe363f37f269ab39
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-09-09 14:34:29 +0900

    use Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+const Food = ({ menu }) => {
+  return <h1>I like {menu}</h1>;
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+        <Food menu="1"></Food>
+        <Food menu="2"></Food>
+        <Food menu="3"></Food>
+        <Food menu="4"></Food>
+      </div>
+    );
+  }
 }
 
 export default App;
