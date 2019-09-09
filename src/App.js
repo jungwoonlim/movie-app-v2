3e0a06ec959bc90e15da63dce26ff33263d4b26d
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-09-09 17:07:18 +0900

    .map Recap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ const Food = ({ name, pic }) => {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={pic} />
+      <img src={pic} alt={name} />
     </div>
   );
 };
 
 const foodLike = [
   {
-    name: "1",
+    id: "1",
+    name: "One",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
-    name: "2",
+    id: "2",
+    name: "Two",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
-    name: "3",
+    id: "3",
+    name: "Three",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   }
@@ -33,16 +36,11 @@ class App extends React.Component {
       <div className="App">
         <h1>Hello React</h1>
         {foodLike.map(dish => (
-          <Food name={dish.name} pic={dish.image}></Food>
+          <Food key={dish.id} name={dish.name} pic={dish.image}></Food>
         ))}
       </div>
     );
   }
 }
-// map ex)
-// const arr = [1,2,3,4];
-// const mapArr = arr.map(x=>x*2);
-// console.log(mapArr);
-// -> [2,4,6,8]
 
 export default App;
