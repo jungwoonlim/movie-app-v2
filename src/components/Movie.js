d5104a05394842e1b401fc90f5cb27a1a69d119c
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-09-10 11:30:28 +0900

    Styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-const Movie = ({ id, year, title, summary, poster }) => {
+const Movie = ({ id, year, title, summary, poster, genres }) => {
   return (
-    <div>
-      <h4>{title}</h4>
-      <img src={poster}></img>
-      <p>{summary}</p>
+    <div className="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
     </div>
   );
 };
@@ -16,7 +27,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
